b3851cbfeda02f798974acade723d6f5fc8d5d60
timon9393 <timon9393@mail.ru>
2024-07-02 18:55:04 +0300

    corrections DZ 7.3_Async programming

--- Lesson 7.3_Async programming/Task 1_sort/main.cpp
+++ Lesson 7.3_Async programming/Task 1_sort/main.cpp
@@ -3,47 +3,46 @@
 #include <future>
 #include <vector>
 
-void AsyncSort(std::promise<std::vector<int>>&& prom_vec, std::vector<int>& arr)
+void FindMin(std::vector<int>& arr, int i, std::promise<int>&& prom)
 {
-	int min;
-	int minIndex;
-	for(int j = 0; j < arr.size(); j++)
+	int min = arr[i];
+	int minIndex = i;
+
+	for (; i < arr.size(); i++)
 	{
-		min = arr[j];
-		minIndex = j;
-		for (int i = j; i < arr.size(); i++)
-		{
-			min = min < arr[i] ? min : arr[i];
-			minIndex = min < arr[i] ? minIndex : i;
-		}
-		if (min < arr[j])
-			std::swap(arr[j], arr[minIndex]);
-		else continue;
-	}
-	
-	prom_vec.set_value(arr);
+		min = min < arr[i] ? min : arr[i];
+		minIndex = min < arr[i] ? minIndex : i;
+	}		
+	prom.set_value(minIndex);
 }
+
+void AsyncSort(std::vector<int>& arr)
+{
+	for (int i = 0; i < arr.size(); i++)
+	{
+		std::promise<int> prom;
+		std::future<int> ft = prom.get_future();
+		std::async(std::launch::async, FindMin, std::ref(arr), i, std::move(prom));
+		auto minInfo = ft.get();
+		if (arr[minInfo] < arr[i])
+			std::swap(arr[i], arr[minInfo]);
+	}
+}
+
 
 int main()
 {
 	std::vector<int> arr{ 5, 3, 48, 10, 4, 50, 1, 5 };
-
-	std::promise<std::vector<int>> prom;
-	std::future<std::vector<int>> ft = prom.get_future();
-		
 	std::cout << "Before sort:" << std::endl;
 	for (const auto& el : arr)
 		std::cout << el << " ";
 	std::cout << std::endl;
-	
-	std::thread th(AsyncSort, std::move(prom), std::ref(arr));	
-	ft.get();
-	
+
+	AsyncSort(arr);
+
 	std::cout << "After sort:" << std::endl;
 	for (const auto& el : arr)
 		std::cout << el << " ";
-
-	th.join();
 
 	return 0;
 }
